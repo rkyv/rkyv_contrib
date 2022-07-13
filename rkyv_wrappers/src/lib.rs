6eab08588bfb7b8280313eed85613be666ef7ec0
zyansheep <zyansheep@protonmail.com>
2022-07-13 13:44:46 -0400

    add CustomPhantom wrapper

--- rkyv_wrappers/src/lib.rs
+++ rkyv_wrappers/src/lib.rs
@@ -1,10 +1,11 @@
 //! Accessory and community-contributed wrapper types for [rkyv](https://github.com/rkyv/rkyv).
 
-#![deny(broken_intra_doc_links)]
+#![deny(rustdoc::broken_intra_doc_links)]
 #![deny(missing_docs)]
-#![deny(missing_crate_level_docs)]
+#![deny(rustdoc::missing_crate_level_docs)]
 
 pub mod as_hashmap;
+pub mod custom_phantom;
 
 #[cfg(test)]
 pub mod tests;
